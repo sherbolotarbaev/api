1fc59230ea1bd5d4ac671235027179a42ce06187
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-06-13 03:03:14 +0600

    update: ip decorator

--- src/decorators/ip.decorator.ts
+++ src/decorators/ip.decorator.ts
@@ -5,14 +5,11 @@ import type { Request } from 'express';
 export const Ip = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Request>();
-    let ip = request.headers['X-Forwarded-For'];
-
-    if (!ip) {
-      ip =
-        request.headers['x-real-ip'] ||
-        request.socket.remoteAddress ||
-        '127.0.0.1';
-    }
+    const ip =
+      request.headers['x-forwarded-for'] ||
+      request.headers['x-real-ip'] ||
+      request.socket.remoteAddress ||
+      '127.0.0.1';
 
     const ipAddress = Array.isArray(ip) ? ip[0] : ip;
     return ipAddress;

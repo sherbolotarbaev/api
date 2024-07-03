fbd544c06aeca69e0dbc416e92acb16a564e246c
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-07-04 03:53:14 +0600

    update: create-user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -8,21 +8,18 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @IsOptional()
   @IsNotEmpty({ message: 'Name cannot be empty.' })
   @IsString({ message: 'Name must be a string.' })
   @MinLength(2, { message: 'Name must be at least 2 characters long.' })
   @MaxLength(64, { message: 'Name cannot be longer than 64 characters.' })
   name: string;
 
-  @IsOptional()
   @IsNotEmpty({ message: 'Surname cannot be empty.' })
   @IsString({ message: 'Surname must be a string.' })
   @MinLength(2, { message: 'Surname must be at least 2 characters long.' })
   @MaxLength(64, { message: 'Surname cannot be longer than 64 characters.' })
   surname: string;
 
-  @IsOptional()
   @IsNotEmpty({ message: 'Email cannot be empty.' })
   @IsEmail({}, { message: 'Invalid email.' })
   email: string;

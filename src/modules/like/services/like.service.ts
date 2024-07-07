3a1ae54301d8b9e8c7b01ba1b1d9169e5b3536ae
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-07-08 05:09:44 +0600

    update: add like

--- src/modules/like/services/like.service.ts
+++ src/modules/like/services/like.service.ts
@@ -17,7 +17,7 @@ export class LikeService {
 
   constructor(private readonly prisma: PrismaService) {}
 
-  async addLike(user: IUser, slug: string): Promise<ILike> {
+  async addLike(user: IUser, slug: string): Promise<ILike[]> {
     const post = await this.prisma.view.findUnique({
       where: {
         slug,
@@ -53,10 +53,16 @@ export class LikeService {
         },
       });
 
-      return this.prisma.like.create({
+      await this.prisma.like.create({
         data: {
           slug,
           userId: user.id,
+        },
+      });
+
+      return this.prisma.like.findMany({
+        where: {
+          slug,
         },
       });
     } catch (error) {
